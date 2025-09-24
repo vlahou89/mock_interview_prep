ab3d03e4cf12caf40317b6987ccd9b3e1d642320
Rita <rita@Ritas-MacBook-Air.local>
2025-09-24 11:36:59 +0100

    update variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDB93ti-MEzF-z_EFyIvYYpm-ImMKYmN-4",
-  authDomain: "mockinterviewprep-22f61.firebaseapp.com",
-  projectId: "mockinterviewprep-22f61",
-  storageBucket: "mockinterviewprep-22f61.firebasestorage.app",
-  messagingSenderId: "384170535495",
-  appId: "1:384170535495:web:6a4868255865731c231f62",
-  measurementId: "G-79TMCW4RJJ",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
